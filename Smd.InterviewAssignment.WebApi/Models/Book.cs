2547fa8075dbb8ea49fa4156495fce5b9b22fed7
nefrix <wo.janas@gmail.com>
2023-01-07 13:48:47 +0100

    Added Required attributes for Book model

--- Smd.InterviewAssignment.WebApi/Models/Book.cs
+++ Smd.InterviewAssignment.WebApi/Models/Book.cs
@@ -1,9 +1,14 @@
-﻿namespace Smd.InterviewAssignment.WebApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Smd.InterviewAssignment.WebApi.Models
 {
     public class Book
     {
+        [Required]
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; }
+        [Required]
         public string Author { get; set; }
     }
 }
